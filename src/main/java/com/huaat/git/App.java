4acd779f54c7ccc669e5525316f5b4ec75c00061
feifeizhou0659 <feifei.zhou@huaat.net>
2018-06-09 12:24:38 +0800

    first commit

---
+++ src/main/java/com/huaat/git/App.java
@@ -0,0 +1,13 @@
+package com.huaat.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
